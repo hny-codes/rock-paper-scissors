76a0a92616f1446e519763031aba53df4a2197f2
Henry <hnguyhenry@protonmail.com>
2023-01-08 15:45:39 -0800

    Add function to play the games 5 times

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const playRound = (playerSelection, computerSelection) => {
   return (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
-    ? "Player 1 won!"
+    ? "You won!"
     : (playerSelection == "rock" && computerSelection == "paper") ||
       (playerSelection == "scissors" && computerSelection == "rock") ||
       (playerSelection == "paper" && computerSelection == "scissors")
-    ? "Player 2 won!"
+    ? "CPU won!"
     : "Draw!";
 };
 
@@ -16,3 +16,17 @@ const computerChoice = () => {
   const choice = Math.floor(Math.random() * 3);
   return choice === 0 ? "rock" : choice === 1 ? "paper" : "scissors";
 };
+
+const game = () => {
+  console.log("Let's play 5 rounds!");
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Pick your hand! (Rock, paper, scissors) ");
+    let cpuSelection = computerChoice();
+    console.log(
+      `Your choice: ${playerSelection} | CPU Choice: ${cpuSelection}`
+    );
+    console.log(playRound(playerSelection, cpuSelection));
+  }
+};
+
+game();
