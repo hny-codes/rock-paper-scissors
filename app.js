58b0eac8c0ea7c10729a2c77e7eeff5b7088d75f
Henry <hnguyhenry@protonmail.com>
2023-01-16 11:41:59 -0800

    Add gameplay event listeners to display match results

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// Create element to track amount of games
+let gameTracker = 0;
+let playerScoreNum = 0;
+let enemyScoreNum = 0;
+
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   return (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
-    ? "You won!"
+    ? 1
     : (playerSelection == "rock" && computerSelection == "paper") ||
       (playerSelection == "scissors" && computerSelection == "rock") ||
       (playerSelection == "paper" && computerSelection == "scissors")
-    ? "CPU won!"
+    ? 0
     : "Draw!";
 };
 
@@ -17,16 +22,62 @@ const computerChoice = () => {
   return choice === 0 ? "rock" : choice === 1 ? "paper" : "scissors";
 };
 
-const game = () => {
-  console.log("Let's play 5 rounds!");
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Pick your hand! (Rock, paper, scissors) ");
-    let cpuSelection = computerChoice();
-    console.log(
-      `Your choice: ${playerSelection} | CPU Choice: ${cpuSelection}`
-    );
-    console.log(playRound(playerSelection, cpuSelection));
+// Grab buttons
+const buttons = document.querySelectorAll(".game-choice");
+
+// Event listener to play a match
+const playGame = (e) => {
+  const playerScore = document.querySelector(".player-score");
+  const enemyScore = document.querySelector(".enemy-score");
+  const matchup = document.querySelector(".matchup");
+
+  let result = playRound(e.target.innerText, computerChoice());
+  if (result === 1) {
+    playerScoreNum += 1;
+    playerScore.textContent = `Your Score: ${playerScoreNum}`;
+    matchup.textContent = `You won!`;
+  } else if (result === 0) {
+    enemyScoreNum += 1;
+    enemyScore.textContent = `Enemy Score: ${enemyScoreNum}`;
+    matchup.textContent = `You lose!`;
+  } else {
+    matchup.textContent = `Draw!`;
   }
+
+  gameTracker += 1;
+  console.log(gameTracker);
+};
+
+// Event listener to remove the game when 5 games have been played
+const gameTrack = (e) => {
+  const matchup = document.querySelector(".matchup");
+
+  if (gameTracker == 5) {
+    console.log("Remove!");
+    buttons.forEach((button) => {
+      button.removeEventListener("click", playGame);
+    });
+
+    matchup.textContent =
+      playerScoreNum > enemyScoreNum
+        ? `VICTORY!`
+        : playerScoreNum < enemyScoreNum
+        ? `DEFEAT!`
+        : `DRAW!`;
+  }
+};
+
+const game = () => {
+  const playerScore = document.querySelector(".player-score");
+  const enemyScore = document.querySelector(".enemy-score");
+
+  playerScore.textContent += playerScoreNum;
+  enemyScore.textContent += enemyScoreNum;
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", playGame);
+    button.addEventListener("click", gameTrack);
+  });
 };
 
 game();
